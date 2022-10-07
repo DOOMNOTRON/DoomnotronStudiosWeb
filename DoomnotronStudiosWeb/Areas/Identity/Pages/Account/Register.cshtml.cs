0f39341e9e961b7d6e61f106a926741183b84ff3
DOOMNOTRON <edgarcruztacoma@gmail.com>
2022-10-07 09:02:16 -0700

    All new users get the User role.

--- DoomnotronStudiosWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ DoomnotronStudiosWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -11,6 +11,7 @@ using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading;
 using System.Threading.Tasks;
+using DoomnotronStudiosWeb.Models;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
@@ -126,6 +127,9 @@ namespace DoomnotronStudiosWeb.Areas.Identity.Pages.Account
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
+
+                    // Adds new account to the User role
+                    await _userManager.AddToRoleAsync(user, IdentityHelper.User);
 
                     var userId = await _userManager.GetUserIdAsync(user);
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
